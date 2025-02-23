4331709ce70dd2783a6fdb37af19b988ae17b6f9
Karthikeyan <140417175+Karthikeyan-J1@users.noreply.github.com>
2025-02-23 14:51:16 +0530

    Create DAY1-CODE-1.v

---
+++ WEEK ONE TASK/DAY1/DAY1-CODE-1.v
@@ -0,0 +1,14 @@
+1.STEP ONE:
+module top_module( output one );
+
+// Insert your code here
+    assign one = 1;
+
+endmodule
+
+2.ZERO:
+module top_module(
+    output zero
+);// Module body starts after semicolon
+assign zero=0;
+endmodule
